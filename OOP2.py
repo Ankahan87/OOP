9a37adcef6fa36c3e2ab4770e81b47d61460d832
Anna Khan <anuk116@mail.ru>
2023-12-02 15:00:49 +0300

    Initial commit

---
+++ OOP2.py
@@ -0,0 +1,60 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+    def rate_lectors(self, lector, course, grade):
+        if isinstance(lector, Lecturer) and course in self.courses_in_progress and course in lector.courses_attached:
+            if course in lector.student_grades:
+                lector.student_grades[course] += [grade]
+            else:
+                lector.student_grades[course] = [grade]
+        else:
+            return 'Ошибка'
+        
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        
+class Lecturer(Mentor):
+    name = ""
+    surname = ""
+    student_grades = {}
+class Reviewer(Mentor):
+    name = ""
+    surname = ""
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+ 
+cool_mentor = Reviewer('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+ 
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+Lecturer1 = Lecturer("Иван Иваныч", "Иванов")
+Reviewer1 = Reviewer("Петр Петрович", "Петров") 
+print(best_student.grades)
+print(Lecturer1.name, Lecturer1.surname)
+print(Reviewer1.name, Reviewer1.surname)
+
+best_student.rate_lectors(Lecturer1, 'Python', 10)
+print(Lecturer1.student_grades)
+Lecturer1.courses_attached += ['Python']
+best_student.rate_lectors(Lecturer1, 'Python', 10)
+print(Lecturer1.student_grades)
