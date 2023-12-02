428fff0b3db2000c81db781007b374ad3a748177
Anna Khan <anuk116@mail.ru>
2023-12-02 16:00:20 +0300

    Change 3 Task

--- OOP3.py
+++ OOP3.py
@@ -7,8 +7,8 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
     def __str__(self):
-        return f"Имя: {self.name}/nФамилия: {self.surname}/nСредняя оценка за домашние задания: {self.make_average_grade()}/nКурсы в процессе изучения: "
-        +",".join(self.courses_in_progress) + "/nЗавершенные курсы: "+",".join(self.finished_courses)
+        rzd = ","
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.make_average_grade()}\nКурсы в процессе изучения: {rzd.join(self.courses_in_progress)} \nЗавершенные курсы: {rzd.join(self.finished_courses)}"
     def make_average_grade(self):
         all_courses = self.finished_courses + self.courses_in_progress
         sums_grade = 0
@@ -42,7 +42,7 @@ class Lecturer(Mentor):
     surname = ""
     student_grades = {}
     def __str__(self):
-        return f"Имя: {self.name}/nФамилия: {self.surname}/nСредняя оценка за лекции: {self.make_average_grade_lector()}"
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.make_average_grade_lector()}"
     def make_average_grade_lector(self):
         all_courses = self.courses_attached
         sums_grade = 0
@@ -59,7 +59,7 @@ class Reviewer(Mentor):
     name = ""
     surname = ""
     def __str__(self):
-        return f"Имя: {self.name}/nФамилия: {self.surname}"
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -90,3 +90,9 @@ print(Lecturer1.student_grades)
 Lecturer1.courses_attached += ['Python']
 best_student.rate_lectors(Lecturer1, 'Python', 10)
 print(Lecturer1.student_grades)
+best_student.rate_lectors(Lecturer1, 'Python', 8)
+best_student.rate_lectors(Lecturer1, 'Python', 7)
+best_student.rate_lectors(Lecturer1, 'Python', 9)
+print(Lecturer1)
+print(Reviewer1)
+print(best_student)
